585cbd8f01620d311d309e4a25db9ae96ef82569
v-uprykhodzka <v-uprykhodzka@expedia.com>
2018-04-19 03:58:06 +0300

    Some styles tuning. Added loader for messages

--- client/app/common/loader/loader.component.ts
+++ client/app/common/loader/loader.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'chat-loader',
-  template: '<div class="loader"></div>\n',
+  template: '<div class="loader"></div>',
   styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent {
