e2224dc9cf4080a54f28e538ddbfa64c7920c757
v-uprykhodzka <v-uprykhodzka@expedia.com>
2018-04-19 04:21:12 +0300

    Animation improvements

--- client/app/chat/person/person.component.ts
+++ client/app/chat/person/person.component.ts
@@ -1,14 +1,14 @@
-import {Component, EventEmitter, Input, Output, ViewEncapsulation} from '@angular/core';
-import {fadeShiftAnimation} from '../../animations/fade-shift.animation';
+import {Component, Input, ViewEncapsulation} from '@angular/core';
 import {fadeAnimation} from "../../animations/fade.animation";
 import {User} from "../../domain/user";
+import {scaleAnimation} from "../../animations/scale.animation";
 
 @Component({
   selector: 'chat-person',
   templateUrl: './person.component.html',
   styleUrls: ['./person.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  animations: [fadeShiftAnimation, fadeAnimation]
+  animations: [scaleAnimation, fadeAnimation]
 })
 export class PersonComponent {
   @Input() className = '';
